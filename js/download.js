0e9e262411db8b8a908beed5e181cde0cc2ff1e0
snowfleek22 <simonkamau9000@gmail.com>
2024-06-13 11:09:16 +0300

    Add files via upload

---
+++ js/download.js
@@ -0,0 +1,18 @@
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Replace 'path_to_your_file' with the actual path to your file
+    var fileUrl = 'CURRICULUM_VITAE.pdf';
+    var fileName = 'My CV.pdf'; // Change the filename as needed
+
+    fetch(fileUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            var url = window.URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = fileName;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => console.error('There was a problem with the fetch operation:', error));
+});
---
+++ js/script.js
@@ -0,0 +1,78 @@
+
+
+function scrollToAbout() {
+    var aboutSection = document.querySelector('.about');
+    aboutSection.scrollIntoView({behavior: "smooth"})
+    
+}
+
+function scrollToEducation() {
+    var aboutSection = document.querySelector('.education');
+    aboutSection.scrollIntoView({behavior: "smooth"})
+    
+}
+function scrollToPortfolio() {
+    var aboutSection = document.querySelector('.portfolio');
+    aboutSection.scrollIntoView({behavior: "smooth"})
+    
+}
+
+
+/* ************** typing animation ****************** */
+var typed = new Typed(".typing", {
+    strings:["","an Actuary", "a Web designer", "a Web developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+/* ********************************* Aside ************************** */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+    for(let  i=0; i<totalNavList; i++)
+        {
+            const a = navList[i].querySelector("a");
+            a.addEventListener("click", function(){
+                this.classList.add("active")
+            })
+        }
+
+
+document.querySelector(".hire-me").addEventListener("click", function()
+{
+    showSection(this);
+    updateNav(this);
+})
+
+
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
+
+// Use HTTPS
+const httpsOptions = {
+  key: fs.readFileSync('path/to/private/key.pem'),
+  cert: fs.readFileSync('path/to/certificate.pem')
+};
+
+// Middleware to redirect HTTP to HTTPS
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
+// Your routes and other middleware
+// ...
+
+https.createServer(httpsOptions, app).listen(443, () => {
+  console.log('Server running on port 443');
+});
+
+
